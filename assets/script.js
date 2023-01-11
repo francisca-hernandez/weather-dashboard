55c7f7cc4505452b65fc97321faf8dc79afb55af
francisca hernandez <francisca.hernandez17@gmail.com>
2023-01-10 21:26:09 -0800

    updating index.html file and  lib files

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,113 @@
-// empty city arrays
-let cities = [];
+//global variables
+let searchForm = document.querySelector("#search-btn")
+const userInput = document.querySelector("#city-name")
+let citySearch = document.querySelector("#city-search")
 
-// global arrays
-const searchBtn = document.querySelector('#search-btn');
-const userinput = document.querySelector('#city-search');
-const present = document.querySelector('#current-weather');
-// const future = document.querySelector('#
-// const past  = document.querySelector('#
+const currentWeatherEl = document.querySelector("#curweather-container")
+const currentCity = document.querySelector('.current-city')
 
-const apiKey = 'ea13ae0f55f1f14f2b43391b0a36bafa';
+let tempEl = document.querySelector(".temp")
+let humidityEl = document.querySelector(".humidity")
+let windEl = document.querySelector(".wind")
+let iconEl = document.querySelector('.icon')
 
-let searchArr = [];
+let tempForeEl = document.querySelector(".temp-forecast")
+let humidityForeEl = document.querySelector(".humidity-forecast")
+let windForeEl = document.querySelector(".wind-forecast")
 
-function search() {
+const forecastEl = document.querySelector('.date-i')
 
-    // const currentCity =`https://api.openweathermap.org/geo/1.0/direct?q=${userInput.value}&limit=5&appid`; 
+// const API_Key = 'e045f64f4cd0813aba91c0138b34adad'
+const API_Key = '3124a0b815fd42bc0bf779711fb207c0'
 
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citysearch + "&appid=" + apiKey + "&units=imperial";
 
-    fetch(apiUrl)
-           .then(response => response.json())
-            .then(data => {
-                getWeather(data[0].lat, data[0].lon, data[0].name)
-            });
+// // GIVEN a weather dashboard with form inputs
+// // step 1:
+// // WHEN I search for a city
+// // THEN I am presented with current and future conditions for that city and that city is added to the search history
 
-            const form = function(event) {
-                event.preventDefault();
-              
-         
+const searchCityForm = (event) => {
+    event.preventDefault();
+    var cityName = userInput.value.trim();
+
+    if (cityName) {
+        userInput.value = "";
+        var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_Key}`;
+        fetch(apiURL).then(data => data.json())
+        
+        .then(data => {
+            let { lat, lon } = data;
+
+            let url = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&units=imperial&appid=${API_Key}`;
+
+            fetch(url).then(response => {
+                return response.json();
+            })
+
+        
+                .then(data => {
+                    let forecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_Key}`;
+                    fetch(forecast).then(response => {
+                        return response.json();
+                    })
+                        .then(data => {
+                            d = data;
+                            displayCurrentWeather(data);
+                            displayFutureWeather(data);
+                         
+                        })
+                    
+                });
+
+        });
+        
+    } else {
+        alert("Please enter a City Name")
+    }
+};
+
+    // // step 2:
+// // WHEN I view current weather conditions for that city
+// // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+
+
+let displayCurrentWeather = (data) => {
+    const { temp, humidity } = data.list[0].main;
+    // set DOM elements from API
+    currentCity.textContent = `${data.city.name} (${new Date(data.list[0].dt*1000).toDateString()})`;
+    currentCity.innerHTML += `<img src="http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png">`;
+    tempEl.textContent = temp;
+    humidityEl.textContent = humidity;
+    windEl.textContent = data.list[0].wind.speed;
+}
+//step 3:
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+
+let displayFutureWeather = (data) => {
+
+    for (let i = 7; i < data.list.length; i=i+7) {
+        const {temp, humidity} = data.list[i].main;
+
+        forecastEl.textContent = `(${new Date(data.list[i].dt*1000).toDateString()})`;
+       tempForeEl.textContent = temp;
+       humidityForeEl.textContent = humidity;
+       windForeEl.textContent = data.list[i].wind.speed;
+    }
+
+};
+
+// //save to localStorage
+// let saveCity = () => {
+//     localStorage.setItem('city', JSON.stringify(cities));
+// };
+
+// searchForm.addEventListener("submit", searchCityForm);
+
+
+
+
+
+
+
